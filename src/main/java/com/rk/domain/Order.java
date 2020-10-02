72280dca880941f7d1e7391844a43b286c4af473
RomanKl <klisch.roma@gmail.com>
2020-10-03 01:29:36 +0300

    Deploy 03.10.2020: - re-buil sql schemas - added lobok annotation;

--- src/main/java/com/rk/domain/Order.java
+++ src/main/java/com/rk/domain/Order.java
@@ -1,13 +1,20 @@
 package com.rk.domain;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Order {
     private long id;
     private User user;
     private String status;
-    private Map<Glasses,Integer> glassesMap;
     private LocalDateTime orderTime;
+    @EqualsAndHashCode.Exclude
+    private Map<Glasses,Integer> glassesMap;
 
 }
